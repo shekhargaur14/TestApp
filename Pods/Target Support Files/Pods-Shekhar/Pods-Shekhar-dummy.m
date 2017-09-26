f44e1a2e8b9809161946aba6dc75c21e8b6ce87f
Ved Kshirsagar <Ved@Veds-MacBook-Pro.local>
2017-09-27 00:09:05 +0530

    facebook integration

---
+++ Pods/Target Support Files/Pods-Shekhar/Pods-Shekhar-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Shekhar : NSObject
+@end
+@implementation PodsDummy_Pods_Shekhar
+@end
---
+++ Shekhar/LoginVC.swift
@@ -0,0 +1,67 @@
+//
+//  LoginVC.swift
+//  Shekhar
+//
+//  Created by Ved Kshirsagar on 26/09/17.
+//  Copyright © 2017 Ved Kshirsagar. All rights reserved.
+//
+
+import UIKit
+import FacebookCore
+import FacebookLogin
+import FBSDKLoginKit
+class LoginVC: UIViewController {
+
+    var dict : [String : AnyObject]!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func loginWithFacebook(_ sender: Any) {
+        let fbLoginManager : FBSDKLoginManager = FBSDKLoginManager()
+        fbLoginManager.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if (error == nil){
+                let fbloginresult : FBSDKLoginManagerLoginResult = result!
+                if fbloginresult.grantedPermissions != nil {
+                    if(fbloginresult.grantedPermissions.contains("email"))
+                    {
+                        self.getFBUserData()
+                        fbLoginManager.logOut()
+                    }
+                }
+            }
+        }
+    }
+    func getFBUserData(){
+        if((FBSDKAccessToken.current()) != nil){
+            FBSDKGraphRequest(graphPath: "me", parameters: ["fields": "id, name, first_name, last_name, picture.type(large), email"]).start(completionHandler: { (connection, result, error) -> Void in
+                if (error == nil){
+                    self.dict = result as! [String : AnyObject]
+                    print(result!)
+                    print(self.dict)
+                }
+            })
+        }
+    }
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
